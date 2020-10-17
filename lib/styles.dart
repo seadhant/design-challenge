a8620e2d1c5f81c9607f674b86ec58858d5b8533
seadhant <sidhant.education@gmail.com>
2020-10-18 02:09:20 +0530

    curved line left

--- lib/styles.dart
+++ lib/styles.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 const Color mainBlack = Color(0xFF383838);
-const Color lightGrey = Color(0xFF606060);
+const Color lightGrey = Color(0xFF707070);
+const Color lighterGrey = Color(0xFF909090);
 const Color white = Color(0xFFFFFFFF);
